11309cee47caf16eba4a53480d0aacb049f8c38a
Eric Mellino <mellinoe@gmail.com>
2018-06-11 11:23:29 -0700

    Command line parsing and high score tracking

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using McMaster.Extensions.CommandLineUtils;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Memory;
 using System;
@@ -9,24 +10,45 @@ using Veldrid.StartupUtilities;
 
 namespace Snake
 {
+    [Command(Description = "The snake game.")]
     class Program
     {
-        private static GraphicsDevice _gd;
-        private static Sdl2Window _window;
-        private static CommandList _cl;
-        private static RgbaFloat _clearColor = new RgbaFloat(0, 0, 0.2f, 1f);
-        private static SpriteRenderer _spriteRenderer;
-        private static World _world;
-        private static Snake _snake;
-        private static TextRenderer _textRenderer;
-        private const float CellSize = 32;
-        private static readonly Vector2 WorldSize = new Vector2(24, 16);
+        // Command line arguments
+        [Option(Description = "Enable high resolution rendering.", ShortName = "hr")]
+        public bool HighResolution { get; } = false;
+        [Option(Description ="The width of the gameplay grid.")]
+        public int Width { get; } = 24;
+        [Option(Description = "The height of the gameplay grid.")]
+        public int Height { get; } = 16;
+
+        private GraphicsDevice _gd;
+        private Sdl2Window _window;
+        private CommandList _cl;
+        private RgbaFloat _clearColor = new RgbaFloat(0, 0, 0.2f, 1f);
+        private SpriteRenderer _spriteRenderer;
+        private World _world;
+        private Snake _snake;
+        private TextRenderer _textRenderer;
+        private float _cellSize;
+        private Vector2 _worldSize;
+        private int _highScore;
+
+        private const float LowResCellSize = 16;
+        private const float HighResCellSize = 32;
 
         public static int Main(string[] args)
         {
+            return CommandLineApplication.Execute<Program>(args);
+        }
+
+        public int OnExecute()
+        {
+            _cellSize = HighResolution ? HighResCellSize : LowResCellSize;
+            _worldSize = new Vector2(Width, Height);
+
             Configuration.Default.MemoryManager = new SimpleGcMemoryManager();
-            int width = (int)(WorldSize.X * CellSize);
-            int height = (int)(WorldSize.Y * CellSize);
+            int width = (int)(_worldSize.X * _cellSize);
+            int height = (int)(_worldSize.Y * _cellSize);
             WindowCreateInfo wci = new WindowCreateInfo(50, 50, width, height, WindowState.Normal, "Snake");
             GraphicsDeviceOptions options = new GraphicsDeviceOptions();
             _window = new Sdl2Window("Snake", 50, 50, width, height, SDL_WindowFlags.OpenGL, false);
@@ -41,11 +63,12 @@ namespace Snake
 
             _window.Resized += () => _gd.ResizeMainWindow((uint)_window.Width, (uint)_window.Height);
 
-            _world = new World(WorldSize, CellSize);
+            _world = new World(_worldSize, _cellSize);
             _snake = new Snake(_world);
             _textRenderer = new TextRenderer(_gd);
             _textRenderer.DrawText("0");
             _snake.ScoreChanged += () => _textRenderer.DrawText(_snake.Score.ToString());
+            _snake.ScoreChanged += () => _highScore = Math.Max(_highScore, _snake.Score);
 
             Stopwatch sw = Stopwatch.StartNew();
             double previousTime = sw.Elapsed.TotalSeconds;
@@ -67,10 +90,11 @@ namespace Snake
 
             _gd.Dispose();
 
-            return 0;
+            Console.WriteLine($"Thanks for playing! Your high score was {_highScore}.");
+            return _snake.Score;
         }
 
-        private static void Update(double deltaSeconds)
+        private void Update(double deltaSeconds)
         {
             _snake.Update(deltaSeconds);
 
@@ -81,7 +105,7 @@ namespace Snake
             }
         }
 
-        private static void DrawFrame()
+        private void DrawFrame()
         {
             _cl.Begin();
             _cl.SetFramebuffer(_gd.MainSwapchain.Framebuffer);
